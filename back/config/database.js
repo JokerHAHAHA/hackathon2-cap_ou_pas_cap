6eb547f5cdf1ffe38b1d6ecb476bac4fc2d32182
JokerHAHAHA <liondejude@hotmail.com>
2016-05-03 19:30:30 +0200

    homeGame view finished

--- back/config/database.js
+++ back/config/database.js
@@ -1,4 +1,4 @@
 // DATABASE CONFIG
 var mongoose = require('mongoose');
-module.exports = mongoose.connect('mongodb://localhost/capAngular');
+module.exports = mongoose.connect('mongodb://localhost/cap');
 
--- front/www/js/controllers/homeGameCtrl.js
+++ front/www/js/controllers/homeGameCtrl.js
@@ -1,3 +1,18 @@
 function homeGameCtrl($scope, $http) {
+	$scope.step = 1;
+	$scope.career = {};
 
+	function load () {
+		 $http.get('http://localhost:8000/careers').then(function (res) {
+		 	$scope.careers = res.data;
+		 	console.log($scope.careers); 
+		 });
+	}
+
+	$scope.add = function () {
+		 var data = $scope.career;
+		 $http.post('http://localhost:8000/careers', data);
+		 $scope.career = {};
+	}
+	load();
 }
