fa501d3fd4c4dad4fbefda53c0c8e8985a28b4b2
Kikucheeseful <gregorybizet@gmail.com>
2016-05-03 21:18:20 +0200

    compte & signup html

--- front/www/js/routes.js
+++ front/www/js/routes.js
@@ -1,59 +1,69 @@
 angular.module('app.routes', [])
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function ($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
 
-  
 
-  .state('home', {
-    url: '/page1',
-    templateUrl: 'templates/home.html',
-    controller: 'homeCtrl'
-  })
 
-  .state('menu.homeGame', {
-    url: 'page2',
-    views: {
-      'side-menu21': {
-        templateUrl: 'templates/homeGame.html',
-        controller: 'homeGameCtrl'
-      }
-    }
-  })
+        .state('home', {
+        url: '/page1',
+        templateUrl: 'templates/home.html',
+        controller: 'homeCtrl'
+    })
 
-  .state('menu.compte', {
-    url: 'page3',
-    views: {
-      'side-menu21': {
-        templateUrl: 'templates/compte.html',
-        controller: 'compteCtrl'
-      }
-    }
-  })
+    .state('menu.homeGame', {
+        url: 'page2',
+        views: {
+            'side-menu21': {
+                templateUrl: 'templates/homeGame.html',
+                controller: 'homeGameCtrl'
+            }
+        }
+    })
 
-  .state('menu', {
-    url: '/',
-    templateUrl: 'templates/menu.html',
-    abstract:true
-  })
+    .state('menu.compte', {
+        url: 'compte',
+        views: {
+            'side-menu21': {
+                templateUrl: 'templates/compte.html',
+                controller: 'compteCtrl'
+            }
+        }
+    })
 
-  .state('menu.chat', {
-    url: 'page4',
-    views: {
-      'side-menu21': {
-        templateUrl: 'templates/chat.html',
-        controller: 'chatCtrl'
-      }
-    }
-  })
+    .state('menu', {
+        url: '/',
+        templateUrl: 'templates/menu.html',
+        abstract: true
+    })
 
-  $urlRouterProvider.otherwise('/page1')
+    .state('menu.chat', {
+        url: 'page4',
+        views: {
+            'side-menu21': {
+                templateUrl: 'templates/chat.html',
+                controller: 'chatCtrl'
+            }
+        }
+    })
 
-  
+    .state('menu.signup', {
+        url: 'signup',
+        views: {
+            'side-menu21': {
+                templateUrl: 'templates/signup.html',
+                controller: 'homeCtrl'
+            }
+        }
+    })
+
+    // $urlRouterProvider.otherwise('/signup')
+
+
 
 });
