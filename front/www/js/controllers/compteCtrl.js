f73e34cc5f9278d9a2636cc54a903738cbfdae5a
Kikucheeseful <gregorybizet@gmail.com>
2016-05-04 09:44:09 +0200

    some modifs

--- front/www/js/controllers/compteCtrl.js
+++ front/www/js/controllers/compteCtrl.js
@@ -1,3 +1,9 @@
 function compteCtrl($scope, $http) {
+    $scope.user = {};
+    $scope.add = function () {
+        var data = $scope.user;
+        $http.post('http://localhost:8000/users', data);
+        $scope.user = {};
+    }
 
 }
--- front/www/js/controllers/homeCtrl.js
+++ front/www/js/controllers/homeCtrl.js
@@ -1,3 +1,14 @@
-function homeCtrl($scope, $http) {
+function homeCtrl($scope, homeService, $ionicPopup, $state) {
+    $scope.data = {};
 
+    $scope.login = function () {
+        LoginService.loginUser($scope.data.username, $scope.data.password).success(function (data) {
+            $state.go('state.homeGame');
+        }).error(function (data) {
+            var alertPopup = $ionicPopup.alert({
+                title: 'Login failed!',
+                template: 'Please check your credentials!'
+            });
+        });
+    }
 }
